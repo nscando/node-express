d33e5f077ccbd3bc7fd1a1b74f3f9f2954b72257
Nicolas Scandizzo <nicolas.scandizzo@gmail.com>
2020-07-18 18:54:42 -0300

    Auth - JWT - bcrypt - mongoDB-

--- models/userModels.js
+++ models/userModels.js
@@ -1,7 +1,7 @@
 const mongoose = require('../bin/mongodb');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-//Define a schema
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -22,7 +22,7 @@ const UserSchema = new Schema({
           required: true
      }
 });
-// hash user password before saving into database
+
 UserSchema.pre('save', function (next) {
      this.password = bcrypt.hashSync(this.password, saltRounds);
      next();
